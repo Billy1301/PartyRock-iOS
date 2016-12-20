fd8e5f2c21c425e2c3d7905ea8fbb6b6545f98ba
Billy <mr.bill.ly@gmail.com>
2016-12-20 12:06:49 -0800

    created project

---
+++ Party Rock/Party Rock/MainVC.swift
@@ -0,0 +1,25 @@
+//
+//  MainVC.swift
+//  Party Rock
+//
+//  Created by Billy on 12/20/16.
+//  Copyright © 2016 Billy. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Party Rock/Party Rock/PartyCellTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  PartyCellTableViewCell.swift
+//  Party Rock
+//
+//  Created by Billy on 12/20/16.
+//  Copyright © 2016 Billy. All rights reserved.
+//
+
+import UIKit
+
+class PartyCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    func updateUI() {
+        
+    }
+
+}
